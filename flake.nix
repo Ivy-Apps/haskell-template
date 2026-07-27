ffb1d3972b9958e5b79f7be1b8c41ada6b23d38d
Iliyan Germanov <iliyan.germanov971@gmail.com>
2026-07-28 01:06:44 +0300

    Haskell template v1.1.0 (#11)
    * just lint
    
    * Add `todo`
    
    * Add "prop" test util
    
    * Add `template-haskell` dep
    
    * Pin Nix 26.05 and follow nixpkgs
    
    * Format nix

--- flake.nix
+++ flake.nix
@@ -14,8 +14,11 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
